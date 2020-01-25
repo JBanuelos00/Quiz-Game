dcf83d92f2f2a6bd823ecda1156c5f4a78ede98b
Player 1 <j.banuelos00@gmail.com>
2020-01-25 01:13:36 -0800

    created my index.html, style.css, and script.js files. Put css file in assets/styles folder path. Create assets folder with styles and images folders within

---
+++ script.js
@@ -0,0 +1,84 @@
+//shorten your get elements with variables
+const start = $("#start");
+const quizImg = $("#quizImg");
+const quizQuestion = $("#quizQuestion");
+const optionA = $("#optionA");
+const optionB = $("#optionB");
+const optionC = $("#optionC");
+const optionD = $("#optionD");
+
+//create your question arrays
+
+let questions1 = [{
+    question: "",
+    questionImg: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+
+}];
+
+let questions2 = [{
+    question: "",
+    questionImg: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+
+}];
+
+let questions3 = [{
+    question: "",
+    questionImg: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+
+}];
+
+let questions4 = [{
+    question: "",
+    questionImg: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+
+}];
+
+let questions5 = [{
+    question: "",
+    questionImg: "",
+    optionA: "",
+    optionB: "",
+    optionC: "",
+    optionD: "",
+
+}];
+
+//Play button interaction
+function playQuiz() {
+
+    initializeTimer();
+}
+
+
+//initialize your timer and quiz
+function initializeTimer() {
+
+    
+}
+
+//get the right objects for the appropriate question
+    //create on click events for when the player chooses
+        //if timer reaches 0, mark wrong
+        //check to see if the answer is right
+            //if right, do something
+            //if wrong take away something
+            //Move to the next question
+
+//Check overall score
+    //if less than some percentage display image
